ba34a1e5ddb0d7c08e35fd17746fa8321378d91f
avnick89 <avnick89@gmail.com>
2024-06-10 20:36:07 -0500

    First Homie

---
+++ HW.py
@@ -0,0 +1,27 @@
+weather = "sunny"
+if weather == "sunny":
+ print("Wear sunglasses!")
+else:
+ print("Take an umbrella")
+
+A = "Hello., World!"
+print (A)
+type(A)
+
+B = "23"
+print (B)
+type(B)
+
+C = "3.14"
+print (C)
+type(C)
+
+D = "True"
+print (D)
+type(D)
+
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+groceries = bread + peanut_butter + jelly
+print (groceries)
