da6baf25eda87a33499707d70eab06ebccefeeef
avnick89 <avnick89@gmail.com>
2024-06-21 10:36:29 -0500

    second commit

--- HW.py
+++ HW.py
@@ -1,24 +1,46 @@
-weather = "sunny"
-if weather == "sunny":
- print("Wear sunglasses!")
+# 1.Showcase your Dance moves
+
+weather = 'sunny'
+if weather == 'sunny':
+      print('Wear sunglasses')
 else:
- print("Take an umbrella")
+      print('Take an umbrella!')
+
+#Task 2 Your mood today
+Feeling = input("How are you feeling today?")
+if Feeling == "happy":
+      print ("Thats great to hear")
+elif Feeling == "sad":
+      print("I hope your day gets better")
+else:
+      print("invalid response! Try again")
+
+
+
+
+
+
+
+
+
+
+
+
+#2. Python data types and functions
 
 A = "Hello., World!"
-print (A)
-type(A)
+print (type(A))
 
 B = "23"
-print (B)
-type(B)
+print (type(B))
 
 C = "3.14"
-print (C)
-type(C)
+print (type(C))
 
 D = "True"
-print (D)
-type(D)
+print (type(D))
+
+# 3. Arthmetric Operations in Daily Life
 
 bread = 2.75
 peanut_butter = 4.50
